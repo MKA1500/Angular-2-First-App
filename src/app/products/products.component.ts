f3e73852e9f8e47b7b9a4aeda9325ed64cceece3
MKA1500 <maciejkabsch@gmail.com>
2018-05-01 21:44:11 +0200

    products list

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = [
+    {
+      id: 1,
+      name: 'iPhone',
+      price: 650
+    },
+    {
+      id: 2,
+      name: 'iPad',
+      price: 850
+    },
+    {
+      id: 3,
+      name: 'MacBook',
+      price: 850
+    }
+  ]
+
+  selectedProduct: Product
 
   constructor() { }
 
+  onSelectProduct(product) {
+    this.selectedProduct = product
+  }
+
   ngOnInit() {
+    this.selectedProduct = this.products[0]
   }
 
 }
