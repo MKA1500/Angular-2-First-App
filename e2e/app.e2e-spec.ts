6b0c5297aa0e78306c979744a220b67956864caa
MKA1500 <maciejkabsch@gmail.com>
2018-05-01 18:17:50 +0200

    Angular 5 new init

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { FirstngappPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('firstngapp App', function() {
-  let page: FirstngappPage;
+describe('my-app App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new FirstngappPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
